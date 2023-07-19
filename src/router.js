be477c15cb4884b949190263737abc9c83c90174
StefanoGalandrini <stefe68@gmail.com>
2023-07-19 19:15:23 +0200

    Add router.js

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Importare i componenti Vue
+
+/*
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: Home,
+		},
+		{
+			path: '/about',
+			name: 'about',
+			component: About,
+		},
+		{
+			path: '/contacts',
+			name: 'contacts',
+			component: Contacts,
+		},
+	],
+});
+
+export { router };
+
+*/
