fed0ca734ce42159e567713f0094a701f7a4718a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-19 18:45:28 +0200

    solve

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import * as bootstrap from "bootstrap";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
+import { createApp } from "vue";
+import App from "./App.vue";
+import * as bootstrap from "bootstrap";
+
+createApp(App).mount("#app");
