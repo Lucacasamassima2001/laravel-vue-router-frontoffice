4f37b64cd082f4d865e1b1f2a02655658c87077a
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-07-19 18:50:09 +0200

    fix main js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,3 @@ import * as bootstrap from "bootstrap";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
-import { createApp } from "vue";
-import App from "./App.vue";
-import * as bootstrap from "bootstrap";
-
-createApp(App).mount("#app");
