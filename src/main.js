a34344d9ad755ccb75c6dd44e52013cbbb9c7e66
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-07-19 18:38:16 +0200

    removed appvue old

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { createApp } from 'vue'
-import * as bootstrap from "bootstrap";
-import App from './App.vue'
-=======
 import { createApp } from "vue";
 import App from "./App.vue";
->>>>>>> 7a4392b469408e91f5cc45951a5ee47cfad119f1
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount("#app");
