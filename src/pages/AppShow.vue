10a593e482465f7fd3144aa5a6db39264c650d4e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-20 16:05:50 +0200

    comment

--- src/pages/AppShow.vue
+++ src/pages/AppShow.vue
@@ -1,16 +1,24 @@
-
-
 <script>
+import axios from "axios";
 export default {
-
-}
+  created() {
+    // richiesta axios per i dati del post
+    // axios
+    //   .get(this.store.baseUrl + "api/portfolios/" + this.$route.params.id)
+    //   .then((response) => {
+    //     if (response.data.success) {
+    //       this.portfolio = response.data.results;
+    //     } else {
+    //       // this.$router.push({ name: "page404" });
+    //       this.is404 = true;
+    //     }
+    //   });
+  },
+};
 </script>
-
 
 <template>
   <h1>sono show</h1>
 </template>
 
-<style>
-
-</style>
+<style lang="scss" scoped></style>
