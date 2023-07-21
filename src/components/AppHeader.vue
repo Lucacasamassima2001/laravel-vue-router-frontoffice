f798f4c7aaff74bfb3bdd20d752bd51e26604f3a
DavideFarci <yank92.df@gmail.com>
2023-07-21 12:40:18 +0200

    add bold style in text logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,21 +10,20 @@ export default {
 <template>
   <header class="fixed-top container-fluid">
     <div class="row row-cols row-cols-md row-cols-sm">
-    
       <div class="contents d-flex justify-content-between px-4 z-1">
-      
         <div class="logo d-flex">
           <router-link
             class="logo d-flex text-decoration-none"
             :to="{ name: 'home' }"
           >
             <img src="../../public/favicon-32x32.png" alt="logo-nav" />
-            <div class="text-center text-light fs-4 pt-3 logo_text">MIXTALES</div>
+            <div class="text-center text-light fs-4 pt-3 fw-bold logo_text">
+              MIXTALES
+            </div>
           </router-link>
         </div>
 
         <AppNav />
-
       </div>
     </div>
   </header>
